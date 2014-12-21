259b26fe7106eb4f289394b0dbac7120a851f82e
c2251393 <c2251393@gmail.com>
2014-12-22 00:57:33 +0800

    playing some js and jQuery

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $(".button").click(function(){
+      $("div").hide();
+    });
+});
+/*
+function f()
+{
+  var elem = document.getElementById('test');
+  elem.innerHTML = "JIZZ"+Math.random().toString();
+}
+*/
